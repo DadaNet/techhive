def3ac5828f0339a339f255bbc2a88151c481dee
Dada <dada0net@gmail.com>
2020-08-21 16:31:22 +0300

    1

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+// Импортируем jQuery
+//= ../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+//= ../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../../node_modules/bootstrap/dist/js/bootstrap.js
+
+// Импортируем другие js-файлы
+//= my.js
